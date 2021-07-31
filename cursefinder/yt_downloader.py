298c7861280843cf98ed9a0f4a5e2812f86d7218
Tony <SjasFaceMD@gmail.com>
2021-07-31 03:26:55 +0300

    added a search class and result filtering by viewcount

--- cursefinder/yt_downloader.py
+++ cursefinder/yt_downloader.py
@@ -2,6 +2,10 @@ from __future__ import unicode_literals
 import youtube_dl
 from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
 from os import remove
+from youtubesearchpython import CustomSearch, VideoSortOrder
+from pprint import PrettyPrinter
+
+printer = PrettyPrinter(indent=1)
 
 
 def quick_load(link: str):
@@ -10,7 +14,7 @@ def quick_load(link: str):
         res = ydl.extract_info(
                     link, force_generic_extractor=ydl.params.get('force_generic_extractor', False))
         ydl.download([link])
-        print(res.keys())
+        # print(res.keys())
         filename = f'{res.get("title")}.mp4'
     return filename
 
@@ -27,5 +31,18 @@ def quick_clip(link: str, start: int, end: int):
     remove(f'{filename+link.replace("https://www.youtube.com/watch?v=", "-")}.mp4')
 
 
-quick_load("https://www.youtube.com/watch?v=SSuvPuQM7qc")
-# quick_clip("https://www.youtube.com/watch?v=VFe_7Ra3sJ8", 22, 25)
+def quick_search(query):
+    results = {}
+    customSearch = CustomSearch(query, VideoSortOrder.uploadDate, limit=10)
+    for value in customSearch.result().get("result"):
+        # printer.pprint(value)
+        views = value.get("viewCount")
+        parsed_views = int(views.get("text").split(" ")[0].replace(",", "")) if views.get("text") != "No views" else -1
+        results[value.get("link")] = {"id": value.get("id"), "title": value.get("title"), "views": parsed_views}
+    return results
+
+
+results = quick_search("conspiracy")
+for link in results:
+    quick_load(link) if results.get(link).get("views") < 1000 else None
+# # quick_clip("https://www.youtube.com/watch?v=VFe_7Ra3sJ8", 22, 25)
