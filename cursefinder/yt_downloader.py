479ad5df49305f53ea70e3407e01b375e7c3de03
Tony <SjasFaceMD@gmail.com>
2021-08-02 13:46:04 +0300

    videos aren't saved into cwd anymore

--- cursefinder/yt_downloader.py
+++ cursefinder/yt_downloader.py
@@ -1,18 +1,19 @@
 from __future__ import unicode_literals
 import youtube_dl
 from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
-from os import remove, mkdir
+from os import remove, mkdir, getcwd, path
 from youtubesearchpython import CustomSearch, VideoSortOrder
 
 
-def quick_load(link: str):
-    ydl_opts = {'ffmpeg_location':"./ffmpeg-4.4-full_build/bin"}
+def quick_load(link: str, filename: str):
+    ydl_opts = {'ffmpeg_location':"./ffmpeg-4.4-full_build/bin",
+                'outtmpl': path.join(path.join(getcwd(), "videos"), f'{filename}.mp4')}
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         res = ydl.extract_info(
                     link, force_generic_extractor=ydl.params.get('force_generic_extractor', False))
         ydl.download([link])
         # print(res.keys())
-        filename = f'{res.get("title")}.mp4'
+        filename = f'{filename}.mp4'
     return filename
 
 
@@ -52,8 +53,11 @@ def quick_search(query):
     customSearch = CustomSearch(query, VideoSortOrder.uploadDate, limit=10)
     for value in customSearch.result().get("result"):
         views = value.get("viewCount")
-        parsed_views = int(views.get("text").split(" ")[0].replace(",", "")) if views.get("text") != "No views" else -1
-        results[value.get("link")] = {"id": value.get("id"), "title": value.get("title"), "views": parsed_views}
+        try:
+            parsed_views = int(views.get("text").split(" ")[0].replace(",", "")) if views.get("text") != "No views" else -1
+            results[value.get("link")] = {"id": value.get("id"), "title": value.get("title"), "views": parsed_views}
+        except AttributeError:
+            pass
     return results
 
 
@@ -63,5 +67,6 @@ except FileExistsError:
     pass
 results = quick_search("conspiracy")
 for link in results:
-    quick_load(link) if results.get(link).get("views") < 1000 else None
+    filename = results.get(link).get("title")
+    quick_load(link, filename) if results.get(link).get("views") < 1000 else None
 # # quick_clip("https://www.youtube.com/watch?v=VFe_7Ra3sJ8", 22, 25)
